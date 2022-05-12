861361d53f7458c356194080c9e49f652d48c73a
eugenekim <eugenekim3107@gmail.com>
2022-05-12 02:48:05 -0700

    edited the custom dataset files

---
+++ data/customCSV.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import os
+import numpy as np
+
+os.chdir("/Users/eugenekim/PycharmProjects/asl-alphabet-classification/data")
+
+numerical_map = {"A":0, "B":1, "C":2, "D":3, "del":4, "E":5, "F":6,
+                "G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,
+                "nothing":15,"O":16,"P":17,"Q":18,"R":19,"S":20,
+                 "space":21,"T":22,"U":23,"V":24,"W":25,"X":26,"Y":27,"Z":28}
+
+file_path = "./raw_data"
+train_path = "asl_alphabet_train"
+
+def asl_data_csv(num_image):
+
+    # Final array
+    final = np.array([])
+
+    for letter in sorted(os.listdir(os.path.join(file_path, train_path))):
+
+        count = 0
+
+        if letter == ".DS_Store":
+            continue
+
+        map_letter = numerical_map[letter]
+
+        for image in sorted(
+                os.listdir(os.path.join(file_path, train_path, letter))):
+
+            if count == num_image:
+                break
+
+            if not image.endswith(".jpg"):
+                continue
+
+            count += 1
+
+            if len(final) == 0:
+                final = np.array([image, map_letter])
+
+            else:
+                final = np.vstack((final, np.array([image, map_letter])))
+
+    return final
+
+data = pd.DataFrame(asl_data_csv(50))
+data.to_csv("aslDataset.csv")
--- data/customDataset.py
+++ data/customDataset.py
@@ -1,50 +1,24 @@
-import cv2
 import os
-import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import cv2
 
-os.chdir("/data")
+class ASLDataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
 
-numerical_map = {"A":0, "B":1, "C":2, "D":3, "del":4, "E":5, "F":6,
-                "G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,
-                "nothing":15,"O":16,"P":17,"Q":18,"R":19,"S":20,
-                 "space":21,"T":22,"U":23,"V":24,"W":25,"X":26,"Y":27,"Z":28}
+    def __len(self):
+        return len(self.annotations)
 
-file_path = "./raw_data"
-train_path = "asl_alphabet_train"
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
+        image = cv2.imread(img_path)
+        y_label = torch.tensor(int(self.annotations.iloc[index, -1]))
 
-def asl_data_csv(num_image):
+        if self.transform:
+            image = self.transform(image)
 
-    # Final array
-    final = None
-
-    for letter in sorted(os.listdir(os.path.join(file_path, train_path))):
-
-        count = 0
-
-        if letter == ".DS_Store":
-            continue
-
-        map_letter = numerical_map[letter]
-
-        for image in sorted(
-                os.listdir(os.path.join(file_path, train_path, letter))):
-
-            if count == num_image:
-                break
-
-            if not image.endswith(".jpg"):
-                continue
-
-            read_image = cv2.imread(
-                os.path.join(file_path, train_path, letter, image))
-            temp_image = cv2.cvtColor(read_image, cv2.COLOR_BGR2RGB).flatten()
-            full = np.append(temp_image, map_letter)
-            count += 1
-
-            if final is None:
-                final = full
-
-            else:
-                final = np.vstack((final, full))
-
-    np.savetxt("aslDataset.csv", final, delimiter=",")
+        return image, y_label
