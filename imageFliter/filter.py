c0edd5f039c81b6ed20b7d79952210c3f5388dde
eugenekim <eugenekim3107@gmail.com>
2022-09-24 13:20:28 +0900

    created cnn model file

---
+++ imageFliter/filter.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+def noiseFilter(image):
+    image_copy = image.copy()
+    imFilter = np.array[[]]
+    return image_copy
